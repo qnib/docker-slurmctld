80a4b52aefe74774aca788ebbc79d043e7da9e9d
Christian Kniep <christian@qnib.org>
2015-04-07 17:02:12 +0200

    add epilog/prolog scripts

--- opt/qnib/slurm/bin/prolog.sh
+++ opt/qnib/slurm/bin/prolog.sh
@@ -20,6 +20,12 @@
 #_=/usr/bin/env
 
 curl -X PUT -d "${SLURM_JOB_USER}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/user
+curl -X PUT -d "${SLURM_JOB_GROUP}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/group
+curl -X PUT -d "${SLURM_JOB_GID}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/groupid
+curl -X PUT -d "${SLURM_CLUSTER_NAME}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/clustername
+curl -X PUT -d "${SLURM_JOB_PARTITION}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/jobpartition
+curl -X PUT -d "${SLURM_JOB_UID}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/userid
 curl -X PUT -d "$(date +%s)" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/start
 curl -X PUT -d "${SLURM_JOB_NODELIST}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/nodelist
+curl -X PUT -d "${SLURM_JOB_NAME}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/jobname
 
