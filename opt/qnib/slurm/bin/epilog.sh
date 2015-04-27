ebd8efc86a801700e898ecf32ebc1002f6fe4f4c
Christian Kniep <christian@qnib.org>
2015-04-27 20:12:31 +0200

    epilog send consul KV

--- opt/qnib/slurm/bin/epilog.sh
+++ opt/qnib/slurm/bin/epilog.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-env > /tmp/env.epilog
+curl -X PUT -d "$(date +%s)" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/end
+curl -X PUT -d "${SLURM_JOB_EXIT_CODE2}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/exit_code2
+curl -X PUT -d "${SLURM_JOB_DERIVED_EC}" http://consul.service.consul:8500/v1/kv/slurm/job/${SLURM_JOBID}/derived_ec
