e708beb9f86ec2ee1a501ad16993e8de35e0feab
Christian Kniep <christian@qnib.org>
2015-05-03 13:24:27 +0200

    bugfix scratch-setup

--- opt/qnib/bin/scratchsetup.sh
+++ opt/qnib/bin/scratchsetup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-su -c 'touch /scratch/test' alice
+su -c 'touch /scratch/test_alice' alice
 if [ $? -eq 1 ];then
    chmod 777 /scratch/
 fi
-su -c 'touch /scratch/test' alice
+su -c 'touch /scratch/test_alice' alice
 if [ $? -eq 1 ];then
    exit 1
 fi
-su -c 'touch /scratch/test' john
+su -c 'touch /scratch/test_john' john
 if [ $? -eq 1 ];then
    exit 1
 fi
