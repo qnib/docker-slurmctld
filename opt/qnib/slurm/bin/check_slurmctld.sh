81265df806a428b9bef33d9c9dd42315622ccf28
Christian Kniep <christian@qnib.org>
2016-01-03 18:50:46 +0100

    adjust tests

--- opt/qnib/slurm/bin/check_slurmctld.sh
+++ opt/qnib/slurm/bin/check_slurmctld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -n "Check if 'slurmctld' is running... "
-supervisorctl status slurmctld|grep RUNNING
+supervisorctl status slurmctld|grep RUNNING >/dev/null
 EC=$?
 if [ ${EC} -ne 0 ];then
     echo "[FAIL]"
